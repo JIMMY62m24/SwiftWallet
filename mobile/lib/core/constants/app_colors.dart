2e048385705aa28d481c5bbf97bedc10bc60fcca
JIMMYG2m24 <kaboretaryam@gmail.com>
2025-04-25 22:49:25 +0100

    Add core constants: AppColors and AppTypography

---
+++ mobile/lib/core/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryPurple = Color(0xFF6B4EFF);
+  static const background = Color(0xFFF8F9FA);
+  static const cardDark = Color(0xFF2D3748);
+  static const textDark = Color(0xFF1A202C);
+  static const success = Color(0xFF48BB78);
+  static const error = Color(0xFFE53E3E);
+  static const warning = Color(0xFFECC94B);
+  static const info = Color(0xFF4299E1);
+}
